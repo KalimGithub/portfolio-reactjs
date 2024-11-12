86a5308cf112eb57f79ddd2a7c6b76fa236d4323
KalimGithub <sk22kalim@gmail.com>
2024-11-12 17:49:59 +0530

    projects Edited

--- src/resources/projects.js
+++ src/resources/projects.js
@@ -5,6 +5,7 @@ export const projects = [
       "CryptoTracker is the real time crypto currency tracker you always needed.",
       "Compare different crypto currencies, look at their graphs - prices, total volumes, market cap etc.",
       "Search from the top 100 Crypto currencies in real time and add them to your watchlist aswell.",
+      "Deployed the application on Vercel for reliable and scalable hosting.",
     ],
     image:
       "https://images.unsplash.com/photo-1561736778-92e52a7769ef?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8d2ViJTIwZGV2ZWxvcG1lbnR8ZW58MHx8MHx8fDA%3D",
@@ -15,22 +16,21 @@ export const projects = [
   {
     title: "Youtube Clone",
     description: [
-      "CryptoTracker is the real time crypto currency tracker you always needed.",
-      "Compare different crypto currencies, look at their graphs - prices, total volumes, market cap etc.",
-      "Search from the top 100 Crypto currencies in real time and add them to your watchlist aswell.",
+      "Youtube clone is the clone of live video playing web app YOUTUBE",
+      "This projects provides user to play, pause the videos.",
+      "user can like and comment on their favorite videos.",
     ],
     image:
       "https://images.unsplash.com/photo-1561736778-92e52a7769ef?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8d2ViJTIwZGV2ZWxvcG1lbnR8ZW58MHx8MHx8fDA%3D",
     link: "https://kalimgithub.github.io/Youtube-clone/",
     sourceCode: "https://github.com/KalimGithub/Youtube-clone",
-    technologies: ["HTMl", "CSS", "Javascript"],
+    technologies: ["HTMl", "CSS", "Javascript", "MUI"],
   },
   {
     title: "Google Sheet Clone",
     description: [
-      "CryptoTracker is the real time crypto currency tracker you always needed.",
-      "Compare different crypto currencies, look at their graphs - prices, total volumes, market cap etc.",
-      "Search from the top 100 Crypto currencies in real time and add them to your watchlist aswell.",
+      "This projects is simple sheet editor made up with HTML, CSS and Javascript",
+      "This web app provides functionalty of editing the sheet as like in official sheet app by Google.",
     ],
     image:
       "https://images.unsplash.com/photo-1561736778-92e52a7769ef?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8d2ViJTIwZGV2ZWxvcG1lbnR8ZW58MHx8MHx8fDA%3D",
@@ -41,9 +41,8 @@ export const projects = [
   {
     title: "Blinkit Web Clone",
     description: [
-      "CryptoTracker is the real time crypto currency tracker you always needed.",
-      "Compare different crypto currencies, look at their graphs - prices, total volumes, market cap etc.",
-      "Search from the top 100 Crypto currencies in real time and add them to your watchlist aswell.",
+      "This web app is web clone of popular Ecommerce website of Blinkit",
+      "It is a static website made with HTML, CSS and Javascript",
     ],
     image:
       "https://images.unsplash.com/photo-1561736778-92e52a7769ef?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8d2ViJTIwZGV2ZWxvcG1lbnR8ZW58MHx8MHx8fDA%3D",
